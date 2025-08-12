4e5113f1b4854296cfc7ff80ff751f6926377104
FelipeFabiani <felipefabiani@gmail.com>
2025-08-12 23:12:03 +0100

    User State using Cascade Parameter

--- EventEaseApp/Models/UserSessionState.cs
+++ EventEaseApp/Models/UserSessionState.cs
@@ -1,18 +1,80 @@
 ﻿using FluentValidation;
+using Microsoft.AspNetCore.Components;
+using System.Xml.Linq;
 
 namespace EventEaseApp.Models;
 
 public class UserSessionState
 {
-    public string Name { get; set; } = string.Empty;
-    public string Email { get; set; }  = string.Empty;
-    public DateTime LastActive { get; set; } = DateTime.UtcNow;
-    public string CurrentPage { get; set; } = String.Empty;
-    public bool IsAuthenticated { get; set; } = false;
+    public string Name { get; private set; } = string.Empty;
+    public string Email { get; private set; }  = string.Empty;
+    public DateTime LastActiviteAt { get; private set; } = DateTime.UtcNow;
+    public DateTime PreviousActiviteAt { get; private set; } = DateTime.UtcNow;
+    public string CurrentPage { get; private set; } = String.Empty;
+    public string PreviousPage { get; private set; } = String.Empty;
+    public bool IsAuthenticated { get; private set; } = false;
     public List<EventModel> AttendedEvents { get; set; } = new();
+
+
+    public event Action? OnChange;
+    public void LogIn(string name, string email, NavigationManager nav)
+    {
+        Name = name;
+        Email = email;
+        PreviousActiviteAt = LastActiviteAt = DateTime.UtcNow;
+        PreviousPage = CurrentPage = GetCurrentPage(nav);
+        IsAuthenticated = true;
+        AttendedEvents.Clear();
+        NotifyStateChanged();
+    }
+
+    public void UpdatePage(NavigationManager nav)
+    {
+        PreviousPage = CurrentPage;
+        CurrentPage = GetCurrentPage(nav);
+        PreviousActiviteAt = LastActiviteAt;
+        LastActiviteAt = DateTime.UtcNow;
+
+        NotifyStateChanged();
+    }
+    public void LogOff()
+    {
+        Name = string.Empty;
+        Email = string.Empty;
+        PreviousActiviteAt = LastActiviteAt = DateTime.MinValue;
+        PreviousPage = CurrentPage = string.Empty;
+        IsAuthenticated = false;
+        AttendedEvents.Clear();
+        NotifyStateChanged();
+    }
+    public void NotifyStateChanged()
+    {
+        if (IsAuthenticated)
+        {
+            OnChange?.Invoke();
+        }
+    }
+
+    private string GetCurrentPage(NavigationManager nav)
+    {
+        var currentPage = nav.Uri
+            .Replace(nav.BaseUri, "")
+            .Replace("/", " ");
+        if (string.IsNullOrWhiteSpace(currentPage))
+        {
+            currentPage = "Home";
+        }
+        return currentPage;
+    }
 }
 
-public class UserValidator : AbstractValidator<UserSessionState>
+public class UserLogIn
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+
+}
+public class UserValidator : AbstractValidator<UserLogIn>
 {
     public UserValidator()
     {
--- EventEaseApp/Pages/Shared/EventEaseAppBase.cs
+++ EventEaseApp/Pages/Shared/EventEaseAppBase.cs
@@ -5,23 +5,12 @@ namespace EventEaseApp.Pages.Shared;
 
 public class EventEaseAppBase : ComponentBase
 {
-    [Inject]
-    protected UserSessionState User { get; set; } = default!;
+    [CascadingParameter(Name = "User")] protected UserSessionState User { get; set; } = default!;
 
-    [Inject]
-    protected NavigationManager Navigation { get; set; } = default!;
+    [Inject] protected NavigationManager Navigation { get; set; } = default!;
 
     protected override void OnInitialized()
     {
-        var currentPage = Navigation.Uri
-            .Replace(Navigation.BaseUri, "")
-            .Replace("/", " ");
-        
-        if(string.IsNullOrWhiteSpace(currentPage))
-        {
-            currentPage = "Home";
-        }
-
-        User.CurrentPage = currentPage;
+        User.UpdatePage(Navigation);
     }
 }
